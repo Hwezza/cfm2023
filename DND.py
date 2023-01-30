e90ffd18f6f995235b6e7c36d892683a380b88d3
Harrison W <hbwescott@hotmail.co.uk>
2023-01-30 17:41:18 +0000

    Invited everyone in

---
+++ DND.py
@@ -0,0 +1,7 @@
+# DND
+import random
+def simulate_dice_roll(sides):
+    
+    return random.randint(0,sides)
+
+print(simulate_dice_roll(2))
--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-import Main
+#import Main
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create some data for the x, y, and z axis
+x = np.linspace(-5, 10, 100)
+y = np.linspace(-10, 10, 100)
+z = x**2 + y**2
+
+# Create the 3D plot
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot3D(x, y, z, 'red')
+
+# Show the plot
+plt.show()
