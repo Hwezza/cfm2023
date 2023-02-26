9831348dd6dd40c0c8c3f00b7f8868aa41f9460f
Harrison W <hbwescott@hotmail.co.uk>
2023-02-26 15:37:46 +0000

    Update for everyone

--- Brain.py
+++ Brain.py
@@ -10,6 +10,26 @@ class PyBrain:
         gui.open_program1
 
 
+class ParticleSimulation:
+    #Establish Variables
+    def __init__(name, u, angle, mass, air_resistance, air_density, gravity):
+        self.name = name
+        self.u = u
+        self.angle = angle
+        self.mass = mass
+        self.air_resistance = air_resistance
+        self.air_density = air_density
+        self.gravity = gravity
+'''
+req variables:
+Name
+u
+Angle
+Mass
+Air resistance coeff
+Air density
+Gravity
+'''
 
 #testing
 '''
--- gui.py
+++ gui.py
@@ -6,9 +6,9 @@ def open_menu():
     main_window.geometry("800x450")
 
     # choices:
-    first_option = tkinter.Button(main_window, text = "Volumes of\nRevolution", width = 10, height = 5, command = open_program1)
+    first_option = tkinter.Button(main_window, text = "Particle Catapult", width = 10, height = 5, command = open_program1)
     first_option.place(x=20,y=20)
-    first_label = tkinter.Label(main_window, text= "Volumes of Revolution:\n Mapping the area of the 3D figure created by wrapping a 2D graph around the y (or x) axis.",
+    first_label = tkinter.Label(main_window, text= "Particle catapult:\n ???",
                                 width = 60, height = 5, background= "grey")
     first_label.place(x=180, y=20)
     main_window.mainloop()
@@ -17,7 +17,7 @@ def open_menu():
 ### Program 1 (Volumes of revolution?)
 def open_program1():
     program1_window = tkinter.Tk(screenName=None,  baseName=None,  className='Window',  useTk=1)
-    program1_window.title("Volumes of Revolution")
+    program1_window.title("Particle Catapult")
     program1_window.geometry("800x450")
     
     program1_window.mainloop()
