f5092d579327b08c387b8cfedcae00006ee6d53e
Harrison W <hbwescott@hotmail.co.uk>
2023-03-16 15:45:36 +0000

    Update test.py

--- test.py
+++ test.py
@@ -6,22 +6,23 @@ def create_test_data():
     from numpy import pi
     import random
     from random import randint
-    columns = ("name","u","x","z","angle",'mass','diam','air res','air dens','grav')
-    with open('cfmTestSpreadsheet.csv', 'w') as test_file:
-        writer = csv.writer(test_file)
-        writer.writerow(columns)
-        for i in range(1,20):
-            name = "Example"+str(i)
-            u = randint(0,20)
-            x = randint(0,20)
-            z = randint(0,20)
-            angle = round(random.uniform(0, pi/2), 2)
-            mass = randint(0,20)
-            diam = randint(0,30)
-            air_res = randint(0,300)/100
-            air_dens = randint(0,200)/100
-            grav = randint(1,1000)/100
-            writer.writerow((name, u, x, z, angle, mass, diam, air_res, air_dens, grav))
+    def refreshTestData():
+        columns = ("name","u","x","z","angle",'mass','diam','air res','air dens','grav')
+        with open('cfmTestSpreadsheet.csv', 'w') as test_file:
+            writer = csv.writer(test_file)
+            writer.writerow(columns)
+            for i in range(1,20):
+                name = "Example"+str(i)
+                u = randint(0,20)
+                x = randint(0,20)
+                z = randint(0,20)
+                angle = round(random.uniform(0, pi/2), 2)
+                mass = randint(0,20)
+                diam = randint(0,30)
+                air_res = randint(0,300)/100
+                air_dens = randint(0,200)/100
+                grav = randint(1,1000)/100
+                writer.writerow((name, u, x, z, angle, mass, diam, air_res, air_dens, grav))
 
 
 
