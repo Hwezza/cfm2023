2048c7390f2ffe01cf2e87dd51155acf5eac3cb9
Harrison W <hbwescott@hotmail.co.uk>
2023-03-28 20:35:27 +0100

    test changes

--- test.py
+++ test.py
@@ -1,28 +1,32 @@
 ### test file
+import sys
+from sys import platform
+if platform == "darwin":
+    # OS X
+    sys.path.insert(1, 'macLib')
+elif platform == "win32":
+    # Windows
+    sys.path.insert(1, 'winLibib')
 
-
-def create_test_data():
+def refreshData():
     import csv
     from numpy import pi
     import random
     from random import randint
-    def refreshTestData():
-        columns = ("name","u","x","z","angle",'mass','diam','air res','air dens','grav')
-        with open('cfmTestSpreadsheet.csv', 'w') as test_file:
-            writer = csv.writer(test_file)
-            writer.writerow(columns)
-            for i in range(1,20):
-                name = "Example"+str(i)
-                u = randint(0,20)
-                x = randint(0,20)
-                z = randint(0,20)
-                angle = round(random.uniform(0, pi/2), 2)
-                mass = randint(0,20)
-                diam = randint(0,30)
-                air_res = randint(0,300)/100
-                air_dens = randint(0,200)/100
-                grav = randint(1,1000)/100
-                writer.writerow((name, u, x, z, angle, mass, diam, air_res, air_dens, grav))
-
-
+    columns = ("name","u","x","z","angle",'mass','diam','air res','air dens','grav')
+    with open('cfmTestSpreadsheet.csv', 'w') as test_file:
+        writer = csv.writer(test_file)
+        writer.writerow(columns)
+        for i in range(1,20):
+            name = "Example"+str(i)
+            u = randint(1,200)
+            x = randint(1,200)
+            z = randint(1,200)
+            angle = round(random.uniform(0, pi/2), 2)
+            mass = randint(1,200)
+            diam = randint(1,300)
+            air_res = randint(1,300)/10
+            air_dens = randint(1,200)/10
+            grav = randint(10,1000)/100
+            writer.writerow((name, u, x, z, angle, mass, diam, air_res, air_dens, grav))
 
